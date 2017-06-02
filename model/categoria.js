1bc5a7b9826c77a497fa2f0e78be029acae68a8f
JavierHuertas <jhuertasozuna@gmail.com>
2017-06-02 08:28:55 -0600

    Segunda fase del proyecto web agenda

--- model/categoria.js
+++ model/categoria.js
@@ -25,4 +25,53 @@ categoria.select = function(idCategoria, callback) {
     });
   }
 }
+
+categoria.insert = function(data, callback) {
+  if(database) {
+    database.query("INSERT INTO Categoria SET ?",
+    data, function(error, resultado){
+      if(error) {
+        throw error;
+      } else {
+        callback(null, {"insertId": resultado.insertId});
+      }
+    });
+  }
+}
+categoria.update = function(data, callback) {
+  if(database) {
+    var sql = "UPDATE Categoria SET nombreCategoria = ? WHERE idCategoria = ?";
+    database.query(sql, [data.nombreCategoria, data.idCategoria],
+     function(error, resultado){
+      if(error) {
+        throw error;
+      } else {
+        callback(null, data);
+      }
+    });
+  }
+}
+
+categoria.delete = function(idCategoria, callback) {
+  if(database) {
+    var consulta = "DELETE FROM Categoria WHERE idCategoria = ?";
+    database.query(consulta, idCategoria, 
+      function(error, resultado) {
+      if(error) {
+        throw error;
+      } else {
+        var notifacion = {"Mensaje" : ""};
+
+        if(resultado.affectedRows > 0) {
+          notifacion.Mensaje = "Se elimino la categoria";
+        } else {
+          notifacion.Mensaje = "No se elimino la categoria";
+        }
+
+        callback(null, notifacion);
+      }
+    });
+  }
+}
+
 module.exports = categoria;
--- routes/categoriaRoute.js
+++ routes/categoriaRoute.js
@@ -14,7 +14,6 @@ router.get('/api/categoria', function(req, res) {
 
 router.get('/api/categoria/:idCategoria', function(req, res) {
   var idCategoria = req.params.idCategoria;
-
   categoria.select(idCategoria, function(error, resultado){
     if (typeof resultado !== undefined) {
       res.json(resultado);
@@ -23,6 +22,56 @@ router.get('/api/categoria/:idCategoria', function(req, res) {
     }
   });
 });
+
+router.post('/api/categoria', function(req, res) {
+  var data = {
+    idCategoria : null,
+    nombreCategoria : req.body.nombreCategoria
+  }
+  categoria.insert(data, function(error, resultado){
+    if (resultado && resultado.insertId > 0) {
+      var idCategoria = resultado.insertId;
+      res.redirect("/api/categoria/"+idCategoria);
+    } else {
+      res.json({"Mensaje": "No hay categorias"});
+    }
+  });
+});
+
+router.put('/api/categoria/:idCategoria', function(req, res){
+  var idCategoria = req.params.idCategoria;
+  var data = {
+    idCategoria : req.body.idCategoria,
+    nombreCategoria : req.body.nombreCategoria
+  }
+
+  if(idCategoria == data.idCategoria) {
+    categoria.update(data, function(error, resultado) {
+      if(resultado !== undefined) {
+        res.json(resultado);
+      } else {
+        res.json({"Mensaje": "No se modifico la categoria"});
+      }
+    });
+  } else {
+    res.json({"Mensaje": "No concuerdan los Id's"});
+  }
+});
+
+router.delete('/api/categoria/:idCategoria', function(req, res) {
+  var idCategoria = req.params.idCategoria;
+  categoria.delete(idCategoria, function(error, resultado){
+    if (typeof resultado !== undefined) {
+      res.json(resultado);
+    } else {
+      res.json({"Mensaje": "No se elimino la categoria"});
+    }
+  });
+});
+
+
+
+
 
 
 module.exports = router;
